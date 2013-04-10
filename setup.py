25ef01530157b2363e7685f9d0c247f4f53313eb
Brett Smith <tanktarta@blueyonder.co.uk>
2013-04-10 23:51:02 +0200

    "setupify" pyinputevent

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyinputevent',
+      version='0.1',
+      description='Python interface to Linux input subsystem',
+      author='Robert Thomson',
+      author_email='blogcomments.f3c1@corporatism.org',
+      license="MIT",
+      url='http://blog.corporatism.org/blog/2010/02/',
+      packages= [ 'pyinputevent' ],
+      )
